d83adfa6df36ea290b1aaf885e5a4a4230398840
Ryan Holway <ryanholway@Ryans-MacBook-Pro-6.local>
2019-01-14 16:32:38 -0700

    eda1

--- src/navigant-bs.py
+++ src/navigant-bs.py
@@ -16,10 +16,10 @@ def count_num_of_each_unique(df,column):
 
 
 if __name__ == '__main__':
-    df = pd.read_excel('../data/scott-navigant.xlsx')
-    df['Service_to_Post_Days'] = df['Post_Date'] - df['Service_Date']
-    df = df[df['Financial_Division'] != 140034]
-
+    df = pd.read_excel('../../data/DemoData.xlsx')
+    # df['Service_to_Post_Days'] = df['Post_Date'] - df['Service_Date']
+    # df = df[df['Financial_Division'] != 140034]
+'''
     df.drop(['Service_Area', 'Location_Name', 'Bill_Area', 'Department_Name',
     'Department_Number', 'Transaction_Type', 'Service_Date', 'Post_Date',
     'Procedure_Code', 'Financial_Class', 'Payor', 'CPT_Code_CPT_HCPCS',
@@ -33,11 +33,11 @@ if __name__ == '__main__':
     df['Void_Reversal_Status'] = df['Void_Reversal_Status'].map({'Y': 1, 'N': 0})
     df.drop(df.tail(1).index,inplace=True)
     df['Service_to_Post_Days'] = df['Service_to_Post_Days'].dt.days
-
+'''
     # print(get_uniques_and_nan_count(df, 'NCI_Transaction_Detail'))
     # print(count_num_of_each_unique(df, 'NCI_Transaction_Detail'))
 
     # indices = np.where(df['Financial_Division'].isna())
     # print(indices)
 
-    print(df.shape)
+    # print(df.shape)
